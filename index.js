43e2959bca520fb0fec3c74a2dc1f076fc0dcd21
HeisenberXx <mrwhite8q@gmail.com>
2025-09-15 14:14:30 -0500

    agregando carpeta fonts

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ const io = new Server(server, {connectionStateRecovery: {}})
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 app.use(express.static(join(__dirname, 'public')));
+app.use(express.static(join(__dirname, 'fonts')));
 app.get('/', (req, res) => {
   res.sendFile(join(__dirname, 'index.html'));
 });
